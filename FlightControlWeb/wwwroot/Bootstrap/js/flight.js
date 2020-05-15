868ed7503d507290b9ff0a70e1bfe0b0b3cd19d1
shaiyis <shaiyis@gmail.com>
2020-05-15 16:40:52 +0300

    commit

---
+++ FlightControlWeb/wwwroot/Bootstrap/js/flight.js
@@ -0,0 +1,10 @@
+﻿function getFlights() {
+    fetch("api/Flights?relative_to=<DATE_TIME>&sync_all")
+        .then(response => response.json())
+        .then(appendItem)
+        .catch(error => console.log(error))
+}
+
+function appendItem() {
+    
+}
